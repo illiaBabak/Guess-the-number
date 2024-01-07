16d797252fafbbf90c54a7d2f4b8204d450f0cea
illiaBabak <illiababak776@gmail.com>
2024-01-07 23:49:45 +0100

    Added the exit command and difficulty level selection

--- index.py
+++ index.py
@@ -1,23 +1,67 @@
 import random
 
-def start_game():
-    random_number = random.randint(1, 100)
+EASY = 10
+MEDIUM = 50
+HARD = 100
+
+
+def exit_check(input_text):
+    return input_text.strip().lower() == "exit"
+
+
+def select_difficulty():
+    input_text = input(
+        """Choose the difficulty:\n1 - easy(range from 1 to 10) \n2 - medium(range from 1 to 50)\n3 - hard(range from 1 to 100)\n"""
+    )
+
+    if exit_check(input_text):
+        return
+
+    try:
+        complexity = int(input_text)
+    except ValueError:
+        print("Please enter a valid value.")
+        select_difficulty()
+
+    if complexity == 1:
+        start_game(EASY)
+
+    elif complexity == 2:
+        start_game(MEDIUM)
+
+    elif complexity == 3:
+        start_game(HARD)
+
+    else:
+        print("Please enter a valid value.")
+        select_difficulty()
+
+
+def start_game(n):
+    random_number = random.randint(1, n)
     guess(random_number)
-    
+
 
 def end_game():
-    input('You won! Press enter to restart the game')
-    start_game()
-    
+    input_text = input("You won! Press enter to restart the game")
+
+    if exit_check(input_text):
+        return
+
+    select_difficulty()
+
 
 def guess(random_n):
     while True:
-        input_text = input('Guess the number: ')
-    
+        input_text = input("Guess the number: ")
+
+        if exit_check(input_text):
+            return
+
         try:
             number = int(input_text)
         except ValueError:
-            print('Please enter a valid number.')
+            print("Please enter a valid number.")
             continue
 
         if number == random_n:
@@ -25,16 +69,14 @@ def guess(random_n):
             break
 
         elif number > random_n:
-            print('Less')
-     
+            print("Less")
+
         else:
-            print('More')
+            print("More")
 
 
-input('''Welcome to the game "Guess the Number"!
-Your task is to guess the number guessed by the computer using logic. 
-Minimize the number of attempts to become a winner!
-      
-Press enter to start the game''')
+input(
+    """Welcome to the game "Guess the Number"!\nYour task is to guess the number guessed by the computer using logic. \nMinimize the number of attempts to become a winner!\nType "exit" to stop\n\nPress enter to start the game"""
+)
 
-start_game()
+select_difficulty()
